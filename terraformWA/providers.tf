811cb912b8cb23bcb470c9ac0fb00a26c3606c67
Eloi-T <eloi.terrol@epitech.eu>
2025-11-21 14:37:18 +0100

    Changements terraform pour WA

--- terraformWA/providers.tf
+++ terraformWA/providers.tf
@@ -5,10 +5,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 3.100.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.5.0"
-    }
   }
 }
 
@@ -17,6 +13,3 @@ provider "azurerm" {
   subscription_id = "6b9318b1-2215-418a-b0fd-ba0832e9b333"
   tenant_id       = "901cb4ca-b862-4029-9306-e5cd0f6d9f86"
 }
-
-# (Pas d’options nécessaires pour random)
-provider "random" {}
